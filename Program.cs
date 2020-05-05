df5a73b2a4716fce35565c1852406279ed83c680
Bosbobos <konovalov.matvej@mail.ru>
2020-05-05 17:00:29 +0300

    test Quest4

--- Program.cs
+++ Program.cs
@@ -8,39 +8,55 @@ namespace Quests
         static void Main(string[] args)
         {
             var камушек = new Камень() { ХП = 100, Цвет = "Прозрачный"};
-            var гопота = new List<Атаковальщик>();
-            гопота.Add(new Бот() { ХП = 100 });
-            гопота.Add(new Персонаж() { ХП = 100 });
+            var бот = new Бот() { ХП = 100 };
+            var персонаж = new Персонаж() { ХП = 100 };
+            
+            var телеса = new List<Тело>();
+            телеса.Add(камушек);
+            телеса.Add(бот);
+            телеса.Add(персонаж);
 
-            foreach(var гопник in гопота)
+            var гопота = new List<Живой>();
+            гопота.Add(бот);
+            гопота.Add(персонаж);
+            
+            foreach (var гопник in гопота)
             {
-                гопник.Атаковать(камушек);
-                Console.WriteLine(камушек.ХП);
+                гопник.ВыполнитьТакт(телеса);
             }
         }
     }
-    interface Атаковальщик { void Атаковать(Тело тело); }
+    interface Живой { void ВыполнитьТакт(List<Тело> телеса); }
     abstract class Тело { public decimal ХП; }
     class Камень : Тело
     {
         public string Цвет { get; set; }
     }
 
-    class Бот : Тело, Атаковальщик
+    class Бот : Тело, Живой
     {
-        public void Атаковать(Тело тело)
+        public void ВыполнитьТакт(List<Тело> телеса)
         {
-            тело.ХП -= 30;
+            foreach (var i in телеса)
+            {
+                if (i is Персонаж)
+                    i.ХП -= 30;
+            }
         }
     }
 
-    class Персонаж : Тело, Атаковальщик
+    class Персонаж : Тело, Живой
     {
-        public void Атаковать(Тело тело)
+        public void ВыполнитьТакт(List<Тело> телеса)
         {
-            тело.ХП -= 40;
+            foreach (var i in телеса)
+            {
+                if (i is Бот)
+                    i.ХП -= 40;
+            }
         }
-        public decimal Богатство { get; set; }
+
+    public decimal Богатство { get; set; }
     }
 }
     
